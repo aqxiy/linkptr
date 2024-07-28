ec002dd75e5cb96cff096b782243cc5584ac94d5
AQXIY <159473102+aqxiy@users.noreply.github.com>
2024-07-27 23:23:49 -0500

    Create linkTest.js

---
+++ linkTest.js
@@ -0,0 +1,34 @@
+const pronounsMale=['he','him','his'];
+const pronounsFemale=['she','her','hers'];
+
+// "it" is different
+const articles=['a','an','the','it?'];
+
+const navNorth=['north','south','east','west'];
+const navMaj=['country','region','street','address','code']
+
+const pets=['cat','dog','fish']
+
+const lit=['letter','word','sentence','paragraph','section','essay','paper','book','series','compendium','lifetime']
+
+const lit2=['genre','theme']
+
+const media=['eye','ear','skin','nose'] // 2 // 2 // 1 // infinite
+
+// eye, ear only perceive, // skin, nose relatedly both perceive and transmit, // mouth differently functionalized
+
+const mStuff=['vision','sound','tactile','smell']
+
+const cmStuff=['picture','song','poke','odor']
+
+const cmsStuff=['mouth','audio'];
+
+// try abbreviations among
+
+// how might one ascribe numbers/primacy and incorporate accordingly
+
+const nutri=['water','meal','vitamin','mineral']
+
+const digits = [0,1,2,3,4,5,6,7,8,9];
+
+const numCombos=[105, 77, 19, 91] // 01 is diff, avoid 10 
